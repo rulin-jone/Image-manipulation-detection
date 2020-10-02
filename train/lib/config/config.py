4023dd89c444d0d8ef852be9c26f156674909b54
rulin-jone <35483935+rulin-jone@users.noreply.github.com>
2020-10-02 13:32:35 +0800

    Add files via upload

---
+++ train/lib/config/config.py
@@ -0,0 +1,140 @@
+import os
+import os.path as osp
+
+import numpy as np
+import tensorflow as tf
+
+FLAGS = tf.app.flags.FLAGS
+FLAGS2 = {}
+
+
+# 通用参数
+FLAGS2["pixel_means"] = np.array([[[102.9801, 115.9465, 122.7717]]])
+tf.app.flags.DEFINE_integer('rng_seed', 3, "tensorflow seed for reproducibility")
+# 使用随机梯度下降（gradient decent）时最好固定随机数种子(rng_seed)，这样你的代码每次运行的结果都一致。这种做法在演示结果、比较算法或debug时特别有效。
+
+
+
+# 使用的网络名参数
+tf.app.flags.DEFINE_string('net', "vgg16", "The network to be used as backbone")
+
+
+# 训练用参数
+tf.app.flags.DEFINE_float('weight_decay', 0.0005, "for regularization, weight decay")
+tf.app.flags.DEFINE_float('learning_rate', 0.001, "learning rate")
+tf.app.flags.DEFINE_float('momentum', 0.9, "momentum")  # 梯度下降的参数
+tf.app.flags.DEFINE_float('gamma', 0.1, "factor for reducing the learning rate")
+
+tf.app.flags.DEFINE_integer('batch_size', 256, "Network batch size during training")
+tf.app.flags.DEFINE_integer('max_iters', 40000, "Max iteration")
+tf.app.flags.DEFINE_integer('step_size', 30000, "Step size for reducing the learning rate")
+# step_size只支持单步
+tf.app.flags.DEFINE_integer('display', 10, "step of showing the training variables during training")
+
+tf.app.flags.DEFINE_string('initializer', "truncated", "Network initialization parameters")
+tf.app.flags.DEFINE_string('pretrained_model', "./data/imagenet_weights/vgg16.ckpt", "pretrained model")
+
+
+
+    # 控制量
+tf.app.flags.DEFINE_boolean('bias_decay', False, "Whether to have weight decay on bias as well")
+tf.app.flags.DEFINE_boolean('double_bias', True, "whether to double the learning rate for bias")
+tf.app.flags.DEFINE_boolean('use_all_gt', True, "whether to use all ground truth bounding boxes for training,"
+                                                "for COCO, setting USE_ALL_GT to False will exclude boxes that are flagged as ''iscrowd''")
+
+tf.app.flags.DEFINE_integer('max_size', 1000, "Max pixel size of the longest side of a scaled input image")
+tf.app.flags.DEFINE_integer('test_max_size', 1000, "Max pixel size of the longest side of a scaled input image")
+tf.app.flags.DEFINE_integer('ims_per_batch', 1, "images to use per minibatch")
+tf.app.flags.DEFINE_integer('snapshot_iterations', 5000, "iteration to make snapshot")
+
+
+FLAGS2["scales"] = (600,)
+FLAGS2["test_scales"] = (600,)
+
+
+# 测试参数
+tf.app.flags.DEFINE_string('test_mode', "top", "test mode for bbox proposal")
+# 有两种，top，nms
+
+
+
+
+
+
+
+
+# RPN参数
+tf.app.flags.DEFINE_float('rpn_negative_overlap', 0.3, "IOU < thresh: negative example")
+tf.app.flags.DEFINE_float('rpn_positive_overlap', 0.7, "IOU >= thresh: positive example")
+tf.app.flags.DEFINE_float('rpn_fg_fraction', 0.5, "Max number of foreground examples")
+tf.app.flags.DEFINE_float('rpn_train_nms_thresh', 0.7, "NMS threshold used on RPN proposals")
+tf.app.flags.DEFINE_float('rpn_test_nms_thresh', 0.7, "NMS threshold used on RPN proposals")
+
+tf.app.flags.DEFINE_integer('rpn_train_pre_nms_top_n', 12000, "Number of top scoring boxes to keep before apply NMS to RPN proposals")
+tf.app.flags.DEFINE_integer('rpn_train_post_nms_top_n', 2000, "Number of top scoring boxes to keep before apply NMS to RPN proposals")
+tf.app.flags.DEFINE_integer('rpn_test_pre_nms_top_n', 6000, "Number of top scoring boxes to keep before apply NMS to RPN proposals")
+tf.app.flags.DEFINE_integer('rpn_test_post_nms_top_n', 300, "Number of top scoring boxes to keep before apply NMS to RPN proposals")
+tf.app.flags.DEFINE_integer('rpn_batchsize', 256, "Total number of examples")
+tf.app.flags.DEFINE_integer('rpn_positive_weight', -1,
+                            'Give the positive RPN examples weight of p * 1 / {num positives} and give negatives a weight of (1 - p).'
+                            'Set to -1.0 to use uniform example weighting')
+tf.app.flags.DEFINE_integer('rpn_top_n', 300, "Only useful when TEST.MODE is 'top', specifies the number of top proposals to select")
+
+tf.app.flags.DEFINE_boolean('rpn_clobber_positives', False, "If an anchor satisfied by positive and negative conditions set to negative")
+
+
+# proposal参数
+tf.app.flags.DEFINE_float('proposal_fg_fraction', 0.25, "Fraction of minibatch that is labeled foreground (i.e. class > 0)")
+tf.app.flags.DEFINE_boolean('proposal_use_gt', False, "Whether to add ground truth boxes to the pool when sampling regions")
+
+
+
+# bounding box参数
+
+tf.app.flags.DEFINE_float('roi_fg_threshold', 0.5, "Overlap threshold for a ROI to be considered foreground (if >= FG_THRESH)")
+tf.app.flags.DEFINE_float('roi_bg_threshold_high', 0.5, "Overlap threshold for a ROI to be considered background (class = 0 if overlap in [LO, HI))")
+tf.app.flags.DEFINE_float('roi_bg_threshold_low', 0.1, "Overlap threshold for a ROI to be considered background (class = 0 if overlap in [LO, HI))")
+
+tf.app.flags.DEFINE_boolean('bbox_normalize_targets_precomputed', True, "# Normalize the targets using 'precomputed' (or made up) means and stdevs (BBOX_NORMALIZE_TARGETS must also be True)")
+tf.app.flags.DEFINE_boolean('test_bbox_reg', True, "Test using bounding-box regressors")
+
+FLAGS2["bbox_inside_weights"] = (1.0, 1.0, 1.0, 1.0)
+FLAGS2["bbox_normalize_means"] = (0.0, 0.0, 0.0, 0.0)
+FLAGS2["bbox_normalize_stds"] = (0.1, 0.1, 0.1, 0.1)
+
+
+# ROI参数
+tf.app.flags.DEFINE_integer('roi_pooling_size', 7, "Size of the pooled region after RoI pooling")
+
+
+# 数据集参数设置
+FLAGS2["root_dir"] = osp.abspath(osp.join(osp.dirname(__file__)))
+print()
+FLAGS2["data_dir"] = osp.abspath(osp.join(FLAGS2["root_dir"], 'data'))
+# 获取config.py的文件路径并组合成默认的文件路径供_get_default_path使用
+
+
+
+
+
+
+def get_output_dir(imdb, weights_filename):
+    """Return the directory where experimental artifacts are placed.
+        If the directory does not exist, it is created.
+
+        A canonical path is built using the name from an imdb and a network
+        (if not None).
+        """
+    outdir = osp.abspath(osp.join(FLAGS2["root_dir"], FLAGS2["root_dir"], 'default', imdb.name))
+    if weights_filename is None:
+        weights_filename = 'default'
+    outdir = osp.join(outdir, weights_filename)
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    return outdir
+
+
+
+
+
+
